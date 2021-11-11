1ba162381478e8b48da06d8cd6c457f40c57d69b
teomarcdhio <mmarcolinishop@gmail.com>
2021-11-11 11:49:31 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
   <title>PHP Test</title>
  </head>
  <body>
-  <?php echo '<p>Hello World</p>'; ?> 
-#<?php 
-#print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
-#?>
+  <?php 
+  print "Your IP address is ".$_SERVER['REMOTE_ADDR'];
+  ?>
  </body>
 </html>
